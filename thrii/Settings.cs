8b9ec76de202d1e83042309adab33fb7ac568575
Asijzar <etharner@gmail.com>
2016-09-27 02:22:41 +1000

    Adding game size.

--- thrii/Settings.cs
+++ thrii/Settings.cs
@@ -4,9 +4,10 @@ namespace thrii
 {
 	static class Settings
 	{
-		public static uint Width = 800;
-		public static uint Height = 600;
+		public static uint Width = 1280;
+		public static uint Height = 960;
 		public static string Name = "THRII";
 		public static string IconPath = "icon.png";
+		public static uint GameSize = 8;
 	}
 }
