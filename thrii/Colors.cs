27d577cd45843f0f2651ab13bd6f07ae561516c6
Asijzar <etharner@gmail.com>
2016-09-27 02:35:49 +1000

    Adding gems colors.

--- thrii/Colors.cs
+++ thrii/Colors.cs
@@ -5,5 +5,10 @@ namespace thrii
 	public static class Colors
 	{
 		public static Color BgColor = new Color(53, 216, 189);
+		public static Color CircleGemColor = new Color(236, 185, 200);
+		public static Color RombGemColor = new Color(228, 185, 236);
+		public static Color PentagonGemColor = new Color(185, 204, 236);
+		public static Color HexagonGemColor = new Color(147, 202, 243);
+		public static Color OctagonGemColor = new Color(239, 246, 194);
 	}
 }
