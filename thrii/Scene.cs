96b3fffee9b7afd26f0c331abbe916fc8cbe69cd
Asijzar <etharner@gmail.com>
2016-09-29 14:58:49 +1000

    Adding settings scene.

--- thrii/Scene.cs
+++ thrii/Scene.cs
@@ -7,10 +7,12 @@ namespace thrii
 	public class Scene
 	{
 		public List<Entity> EntityList { get; }
+		protected Layout layout;
 
 		public Scene()
 		{
 			EntityList = new List<Entity>();
+			layout = new Layout();
 		}
 
 		public void AddEntity(Entity e)
@@ -28,12 +30,15 @@ namespace thrii
 	{
 		public MenuScene()
 		{
-			EntityList.Add(Spawner.CreateBackground(Settings.Width, Settings.Height, 0, 0, Colors.BgColor));
+			EntityList.Add(Spawner.CreateBackground(layout, Settings.Width, Settings.Height, 0, 0, Colors.BgColor, true));
+			EntityList.Add(Spawner.CreateGameSprite(layout));
 
-			var newGameMenuEntry = Spawner.CreateMenuEntry(BaseNames.MenuNewGame);
-			var SettingsMenuEntry = Spawner.CreateMenuEntry(BaseNames.MenuSettings);
+			var newGameMenuEntry = Spawner.CreateMenuEntry(layout, BaseNames.MenuNewGame);
+			var settingsMenuEntry = Spawner.CreateMenuEntry(layout, BaseNames.MenuSettings);
+			var exitMenuEntry = Spawner.CreateMenuEntry(layout, BaseNames.MenuExit);
 
-			newGameMenuEntry.AddRange(SettingsMenuEntry);
+			newGameMenuEntry.AddRange(settingsMenuEntry);
+			newGameMenuEntry.AddRange(exitMenuEntry);
 			foreach (var entity in newGameMenuEntry)
 			{
 				EntityList.Add(entity);
@@ -45,13 +50,14 @@ namespace thrii
 	{
 		public SessionScene()
 		{
-			EntityList.Add(Spawner.CreateBackground(Settings.Width, Settings.Height, 0, 0, Colors.BgColor));
+			EntityList.Add(Spawner.CreateBackground(layout, Settings.Width, Settings.Height, 0, 0, Colors.BgColor, true));
 			EntityList.Add(Spawner.CreateBackground(
-				Layout.SessionBackgroundWidth,
-				Layout.SessionBackgroundHeight, 
-				Layout.SessionBackgroundX,
-				Layout.SessionBackgroundY, 
-				Color.Transparent, 2, 
+				layout,
+				layout.SessionBackgroundWidth,
+				layout.SessionBackgroundHeight, 
+				layout.SessionBackgroundX,
+				layout.SessionBackgroundY, 
+				Color.Transparent, true, 2, 
 				Color.White
 			));
 
@@ -61,13 +67,36 @@ namespace thrii
 			{
 				for (uint j = 0; j < Settings.GameSize; j++)
 				{
-					Shape gem = new GemView((Gems)Enum.GetValues(typeof(Gems)).GetValue(rnd.Next(0, 5))).GemShape;
+					Shape gem = new GemView((Gems)Enum.GetValues(typeof(Gems)).GetValue(rnd.Next(0, 5)), layout).GemShape;
 					EntityList.Add(Spawner.CreateGem(
 						gem, 
-						Layout.SessionBackgroundX + Layout.GemMargin + Layout.GemDistance * i, 
-						Layout.SessionBackgroundY + Layout.GemMargin + Layout.GemDistance * j
+						layout.SessionBackgroundX + layout.GemMargin + layout.GemDistance * i, 
+						layout.SessionBackgroundY + layout.GemMargin + layout.GemDistance * j
 					));
 				}
+			}
+		}
+	}
+
+	public class SettingsScene : Scene
+	{
+		public SettingsScene()
+		{
+			EntityList.Add(Spawner.CreateBackground(layout, Settings.Width, Settings.Height, 0, 0, Colors.BgColor, true));
+			EntityList.Add(Spawner.CreateGameSprite(layout));
+
+			var resolutionMenuOptionEntry = Spawner.CreateOptionEntry(layout, BaseNames.MenuOptionResolution);
+			var gameSizeMenuOptionEntry = Spawner.CreateOptionEntry(layout, BaseNames.MenuOptionGameSize);
+
+			var applyMenuEntry = Spawner.CreateMenuEntry(layout, BaseNames.MenuApply);
+			var backMenuEntry = Spawner.CreateMenuEntry(layout, BaseNames.MenuBack);
+
+			resolutionMenuOptionEntry.AddRange(gameSizeMenuOptionEntry);
+			resolutionMenuOptionEntry.AddRange(applyMenuEntry);
+			resolutionMenuOptionEntry.AddRange(backMenuEntry);
+			foreach (var entity in resolutionMenuOptionEntry)
+			{
+				EntityList.Add(entity);
 			}
 		}
 	}
