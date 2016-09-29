2d9942d1ace0280b0578a6adf0027d77d2e2f288
Asijzar <etharner@gmail.com>
2016-09-29 14:59:30 +1000

    Removing static from layout. Adding new layouts.

--- thrii/Layout.cs
+++ thrii/Layout.cs
@@ -1,26 +1,152 @@
 ﻿namespace thrii
 {
-	public static class Layout
+	public class Layout
 	{
-		public static uint MenuEntryWidth = Settings.Width / 4;
-		public static uint MenuEntryHeight = Settings.Height / 12;
-		public static uint MenuNewGameX = Settings.Width / 2 - MenuEntryWidth / 2;
-		public static uint MenuNewGameY = Settings.Height / 2 - MenuEntryHeight;
-		public static uint MenuNewGameTextX = MenuNewGameX + MenuEntryWidth / 8;
-		public static uint MenuNewGameTextY = MenuNewGameY + MenuEntryHeight / 10;
-		public static uint MenuSettingsX = MenuNewGameX;
-		public static uint MenuSettingsY = Settings.Height / 2 + MenuEntryHeight / 4;
-		public static uint MenuSettingsTextX = MenuSettingsX + MenuEntryWidth / 5;
-		public static uint MenuSettingsTextY = MenuSettingsY + MenuEntryHeight / 10;
+		public uint GameSpriteWidth;
+		public uint GameSpriteHeight;
+		public uint GameSpriteX;
+		public uint GameSpriteY;
 
-		public static uint SessionBackgroundMargin = MenuEntryHeight;
-		public static uint SessionBackgroundWidth = Settings.Width -SessionBackgroundMargin * 5;
-		public static uint SessionBackgroundHeight = SessionBackgroundWidth;
-		public static uint SessionBackgroundX = (Settings.Width - SessionBackgroundWidth) / 2;
-		public static uint SessionBackgroundY = Settings.Height / 20;
+		public uint FontSize;
 
-		public static uint GemMargin = SessionBackgroundWidth / 50;
-		public static uint GemSize = SessionBackgroundWidth / (Settings.GameSize * 2) - GemMargin / Settings.GameSize;
-		public static uint GemDistance = GemSize * 2;
+		public uint MenuEntryWidth;
+		public uint MenuEntryHeight;
+
+		public uint MenuNewGameX;
+		public uint MenuNewGameY;
+		public uint MenuNewGameTextX;
+		public uint MenuNewGameTextY;
+
+		public uint MenuSettingsX;
+		public uint MenuSettingsY;
+		public uint MenuSettingsTextX;
+		public uint MenuSettingsTextY;
+
+		public uint MenuExitX;
+		public uint MenuExitY;
+		public uint MenuExitTextX;
+		public uint MenuExitTextY;
+
+		public uint OptionEntryWidth;
+		public uint OptionEntryHeight;
+		public uint OptionEntryButtonSize;
+		public uint OptionEntryButtonMargin;
+
+		public uint OptionResolutionLabelX;
+		public uint OptionResolutionLabelY;
+		public uint OptionResolutionX;
+		public uint OptionResolutionY;
+		public uint OptionResolutionTextX;
+		public uint OptionResolutionTextY;
+		public uint OptionResolutionLeftButtonX;
+		public uint OptionResolutionLeftButtonY;
+		public uint OptionResolutionRightButtonX;
+		public uint OptionResolutionRightButtonY;
+
+		public uint OptionGameSizeLabelX;
+		public uint OptionGameSizeLabelY;
+		public uint OptionGameSizeX;
+		public uint OptionGameSizeY;
+		public uint OptionGameSizeTextX;
+		public uint OptionGameSizeTextY;
+		public uint OptionGameSizeLeftButtonX; 
+		public uint OptionGameSizeLeftButtonY;
+		public uint OptionGameSizeRightButtonX;
+		public uint OptionGameSizeRightButtonY;
+
+		public uint MenuApplyX;
+		public uint MenuApplyY;
+		public uint MenuApplyTextX;
+		public uint MenuApplyTextY;
+
+		public uint MenuBackX;
+		public uint MenuBackY;
+		public uint MenuBackTextX;
+		public uint MenuBackTextY;
+
+		public uint SessionBackgroundMargin;
+		public uint SessionBackgroundWidth;
+		public uint SessionBackgroundHeight;
+		public uint SessionBackgroundX;
+		public uint SessionBackgroundY;
+
+		public uint GemMargin;
+		public uint GemSize;
+		public uint GemDistance;
+
+		public Layout()
+		{
+			GameSpriteWidth = Settings.Width / 3;
+			GameSpriteHeight = GameSpriteWidth;
+			GameSpriteX = Settings.Width / 2 - GameSpriteWidth / 2;
+			GameSpriteY = Settings.Height / 20;
+
+			FontSize = Settings.Height / 20;
+
+			MenuEntryWidth = Settings.Width / 4;
+			MenuEntryHeight = Settings.Height / 12;
+
+			MenuNewGameX = Settings.Width / 2 - MenuEntryWidth / 2;
+			MenuNewGameY = Settings.Height / 2 - MenuEntryHeight;
+			MenuNewGameTextX = MenuNewGameX + MenuEntryWidth / 8;
+			MenuNewGameTextY = MenuNewGameY + MenuEntryHeight / 10;
+
+			MenuSettingsX = MenuNewGameX;
+			MenuSettingsY = Settings.Height / 2 + MenuEntryHeight / 4;
+			MenuSettingsTextX = MenuSettingsX + MenuEntryWidth / 5 + FontSize / 4;
+			MenuSettingsTextY = MenuSettingsY + MenuEntryHeight / 10;
+
+			MenuExitX = MenuNewGameX;
+			MenuExitY = Settings.Height / 2 + MenuEntryHeight * 2;
+			MenuExitTextX = MenuExitX + MenuEntryWidth / 3 + Settings.Height / 64;
+			MenuExitTextY = MenuExitY + MenuEntryHeight / 10;
+
+			OptionEntryWidth = MenuEntryWidth;
+			OptionEntryHeight = MenuEntryHeight;
+			OptionEntryButtonSize = MenuEntryHeight / 2;
+			OptionEntryButtonMargin = OptionEntryButtonSize;
+
+			OptionResolutionLabelX = MenuNewGameX + MenuEntryWidth / 6;
+			OptionResolutionLabelY = MenuNewGameY;
+			OptionResolutionX = MenuNewGameX;
+			OptionResolutionY = OptionResolutionLabelY + FontSize + FontSize / 3;
+			OptionResolutionTextX = OptionResolutionX + FontSize;
+			OptionResolutionTextY = OptionResolutionY;
+			OptionResolutionLeftButtonX = OptionResolutionX - OptionEntryButtonMargin;
+			OptionResolutionLeftButtonY = OptionResolutionY + OptionEntryButtonSize + OptionEntryButtonSize / 10;
+			OptionResolutionRightButtonX = OptionResolutionX + MenuEntryWidth + OptionEntryButtonMargin;
+			OptionResolutionRightButtonY = OptionResolutionY  + OptionEntryButtonSize - OptionEntryButtonSize / 10;
+
+			OptionGameSizeLabelX = MenuSettingsX + MenuEntryWidth / 7;
+			OptionGameSizeLabelY = OptionResolutionY + MenuEntryHeight;
+			OptionGameSizeX = MenuSettingsX;
+			OptionGameSizeY = OptionGameSizeLabelY + FontSize + FontSize / 3;
+			OptionGameSizeTextX = OptionGameSizeX + FontSize + FontSize * 2;
+			OptionGameSizeTextY = OptionGameSizeY;
+			OptionGameSizeLeftButtonX = OptionGameSizeX - OptionEntryButtonMargin;
+			OptionGameSizeLeftButtonY = OptionGameSizeY  + OptionEntryButtonSize + OptionEntryButtonSize / 10;
+			OptionGameSizeRightButtonX = OptionGameSizeX + MenuEntryWidth + OptionEntryButtonMargin;
+			OptionGameSizeRightButtonY = OptionGameSizeY + OptionEntryButtonSize - OptionEntryButtonSize / 10;
+
+			MenuApplyX = MenuExitX;
+			MenuApplyY = OptionGameSizeY + MenuEntryHeight + MenuEntryHeight / 2;
+			MenuApplyTextX = MenuApplyX + MenuEntryWidth / 3;
+			MenuApplyTextY = MenuApplyY;
+
+			MenuBackX = MenuExitX;
+			MenuBackY = MenuApplyY + MenuEntryHeight + MenuEntryHeight / 4;
+			MenuBackTextX = MenuBackX + MenuEntryWidth / 3;
+			MenuBackTextY = MenuBackY;
+
+			SessionBackgroundMargin = MenuEntryHeight;
+			SessionBackgroundWidth = Settings.Width -SessionBackgroundMargin * 5;
+			SessionBackgroundHeight = SessionBackgroundWidth;
+			SessionBackgroundX = (Settings.Width - SessionBackgroundWidth) / 2;
+			SessionBackgroundY = Settings.Height / 20;
+
+			GemMargin = SessionBackgroundWidth / 50;
+			GemSize = SessionBackgroundWidth / (Settings.GameSize * 2) - GemMargin / Settings.GameSize;
+			GemDistance = GemSize * 2;
+		}
 	}
 }
