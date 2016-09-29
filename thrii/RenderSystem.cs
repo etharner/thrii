6cf5f14e3d95ada89c789ae257d59348861f759e
Asijzar <etharner@gmail.com>
2016-09-30 05:38:55 +1000

    Adding bounding box and non-zero origin setting.

--- thrii/RenderSystem.cs
+++ thrii/RenderSystem.cs
@@ -40,6 +40,22 @@ namespace thrii
 					((Text)target.Display.DisplayObject).DisplayedString = interfaceTargets[target.Entity.Name];
 				}
 
+				CollisionComponent collision = (CollisionComponent)target.Entity.GetComponent("CollisionComponent");
+				if (collision != null)
+				{
+					Shape shape = (Shape)(target.Display.DisplayObject);
+
+					if (target.Entity.Name.BaseName == BaseNames.Gem)
+					{
+						target.Display.DisplayObject.Position = new SFML.System.Vector2f(
+							target.Position.X + shape.GetLocalBounds().Width / 2.0f, 
+							target.Position.Y + shape.GetLocalBounds().Height / 2.0f
+						);
+					}
+
+					collision.BoundingBox = shape.GetGlobalBounds();
+				}
+
 				engine.Renderer.Draw(target.Display.DisplayObject);
 			}
 		}
