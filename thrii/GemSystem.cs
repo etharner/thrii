c49b19877bac6f8b1bf0e1e1083c630539335f66
Asijzar <etharner@gmail.com>
2016-10-01 17:12:45 +1000

    Preventing non-neighbours gems from switching. Neighbour is vertical-horizontal, not diagonal.

--- thrii/GemSystem.cs
+++ thrii/GemSystem.cs
@@ -8,6 +8,7 @@ namespace thrii
 	{
 		List<Node> movementNodes;
 		List<Vector2f> gemGrid;
+		int[] idGrid;
 		Layout layout;
 
 		public GemSystem(Engine e) : base(e)
@@ -15,11 +16,13 @@ namespace thrii
 			movementNodes = new List<Node>();
 			gemGrid = new List<Vector2f>();
 			layout = new Layout();
+			idGrid = new int[Settings.GameSize * Settings.GameSize];
 
 			for (var i = 0; i < Settings.GameSize; i++)
 			{
 				for (var j = 0; j < Settings.GameSize; j++)
 				{
+					idGrid[(int)Settings.GameSize * i + j] = (int)Settings.GameSize * i + j; 
 					gemGrid.Add(new Vector2f(
 						layout.SessionBackgroundX + layout.GemMargin + layout.GemDistance * j,
 						layout.SessionBackgroundY + layout.GemMargin + layout.GemDistance * i
@@ -28,12 +31,51 @@ namespace thrii
 			}
 		}
 
-		Vector2i GetGridPlace(Vector2f coords)
+		Vector2i GetGridPlace(int id)
 		{
-			return new Vector2i(
-				(int)((coords.X - layout.SessionBackgroundX - layout.GemMargin) / layout.GemDistance),
-				(int)((coords.Y - layout.SessionBackgroundY - layout.GemMargin) / layout.GemDistance)
-			);
+			var place = idGrid[id];
+			var y = (int)(place / Settings.GameSize);
+			var x = (int)(place - y * Settings.GameSize);
+
+			return new Vector2i(x, y);
+		}
+
+		List<Vector2i> GetNeighbours(int id)
+		{
+			var neighbours = new List<Vector2i>();
+			Vector2i gridPlace = GetGridPlace(id - 1);
+
+			if (gridPlace.X > 0)
+			{
+				neighbours.Add(new Vector2i(gridPlace.X - 1, gridPlace.Y));
+			}
+			if (gridPlace.X < Settings.GameSize - 1)
+			{
+				neighbours.Add(new Vector2i(gridPlace.X + 1, gridPlace.Y));
+			}
+			if (gridPlace.Y > 0)
+			{
+				neighbours.Add(new Vector2i(gridPlace.X, gridPlace.Y - 1));
+			}
+			if (gridPlace.Y < Settings.GameSize - 1)
+			{
+				neighbours.Add(new Vector2i(gridPlace.X, gridPlace.Y + 1));
+			}
+
+			return neighbours;
+		}
+
+		bool AreNeighbours(int firstId, int secondId)
+		{
+			List<Vector2i> neighbours = GetNeighbours(firstId);
+			foreach (var neighbour in neighbours)
+			{
+				if (idGrid[secondId - 1] == neighbour.Y * Settings.GameSize + neighbour.X) {
+					return true;
+				}
+			}
+
+			return false;
 		}
 
 		void SwitchGems(List<Name> clickedGemsNames, List<MovementNode> clickedGems)
@@ -50,6 +92,11 @@ namespace thrii
 			first = clickedGems[firstClicked];
 			second = clickedGems[1 - firstClicked];
 
+			if (!AreNeighbours(first.Entity.Name.Id, second.Entity.Name.Id))
+			{
+				return;
+			}
+
 			Vector2f newPosition = gemGrid[second.Entity.Name.Id - 1];
 			var animationComponent = (AnimationComponent)first.Entity.GetComponent("AnimationComponent");
 			animationComponent.X = second.Position.X;
@@ -59,6 +106,10 @@ namespace thrii
 			animationComponent = (AnimationComponent)second.Entity.GetComponent("AnimationComponent");
 			animationComponent.X = first.Position.X;
 			animationComponent.Y = first.Position.Y;
+
+			var firstPosition = idGrid[first.Entity.Name.Id - 1];
+			idGrid[first.Entity.Name.Id - 1] = idGrid[second.Entity.Name.Id - 1];
+			idGrid[second.Entity.Name.Id - 1] = firstPosition;
 
 			gemGrid[first.Entity.Name.Id - 1] = gemGrid[second.Entity.Name.Id - 1];
 			gemGrid[second.Entity.Name.Id - 1] = newPosition;
