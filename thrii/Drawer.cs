91a22399e3da726316e441439327866135ba7d9a
Asijzar <etharner@gmail.com>
2016-10-05 02:39:45 +1000

    Changing uint to int.

--- thrii/Drawer.cs
+++ thrii/Drawer.cs
@@ -8,12 +8,12 @@ namespace thrii
 	{
 		public RenderWindow Window { get; set; }
 
-		public Drawer(uint width, uint height, string name, string iconPath)
+		public Drawer(int width, int height, string name, string iconPath)
 		{
 			var windowSettings = new ContextSettings();
 			windowSettings.AntialiasingLevel = 8;
 
-			Window = new RenderWindow(new VideoMode(width, height), name, Styles.Close, windowSettings);
+			Window = new RenderWindow(new VideoMode((uint)width, (uint)height), name, Styles.Close, windowSettings);
 			SetIcon(iconPath);
 			Window.Closed += OnClosed;
 		}
