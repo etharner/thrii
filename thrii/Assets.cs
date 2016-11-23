f42a8b04dde2af123818b5ee4ac6e49c718348a9
etharner <jhis234cip>
2016-11-23 22:19:22 +1000

    Adding assets file pathes.

---
+++ thrii/Assets.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace thrii
+{
+	public static class Assets
+	{
+		public static string Icon = "icon.png";
+		public static string Settings = "settings.xml";
+		public static string Font = "helvetica.otf";
+		public static string GameLogo = "gameLogo.png";
+		public static string CircleBomb = "circleBomb.png";
+		public static string RombBomb = "rombBomb.png";
+		public static string PentagonBomb = "pentagonBomb.png";
+		public static string HexagonBomb = "hexagonBomb.png";
+		public static string OctagonBomb = "octagonBomb.png";
+		public static string CircleDestroyer = "circleDestroyer.png";
+		public static string RombDestroyer = "rombDestroyer.png";
+		public static string PentagonDestroyer = "pentagonDestroyer.png";
+		public static string HexagonDestroyer = "hexagonDestroyer.png";
+		public static string OctagonDestroyer = "octagonDestroyer.png";
+		public static string CircleLine = "circleLine.png";
+		public static string RombLine = "rombLine.png";
+		public static string PentagonLine = "pentagonLine.png";
+		public static string HexagonLine = "hexagonLine.png";
+		public static string OctagonLine = "octagonLine.png";
+		public static List<string> Music = new List<string> {
+			"thrii1.ogg", "thrii2.ogg", "thrii3.ogg", "thrii4.ogg"
+		};
+		public static string DestroySound = "destroy.ogg";
+		public static string BoomSound = "boom.ogg";
+	}
+}
