a383b63475f46dcf9900679e0e80a0ada338be60
etharner <jhis234cip>
2016-11-24 04:43:12 +1000

    Adding folders pathes for assets.

--- thrii/Assets.cs
+++ thrii/Assets.cs
@@ -4,31 +4,39 @@ namespace thrii
 {
 	public static class Assets
 	{
-		public static string Icon = "icon.png";
+		public static string AssetsFolder = "Assets/";
+		public static string TexturesFolder = AssetsFolder + "Textures/";
+		public static string SoundsFolder = AssetsFolder + "Sounds/";
+		public static string FontsFolder = AssetsFolder + "Fonts/";
+
+		public static string Icon = TexturesFolder + "icon.png";
 		public static string Settings = "settings.xml";
 		public static string Progress = "progress.xml";
-		public static string Font = "helvetica.otf";
-		public static string GameLogo = "gameLogo.png";
-		public static string CircleBomb = "circleBomb.png";
-		public static string RombBomb = "rombBomb.png";
-		public static string PentagonBomb = "pentagonBomb.png";
-		public static string HexagonBomb = "hexagonBomb.png";
-		public static string OctagonBomb = "octagonBomb.png";
-		public static string CircleDestroyer = "circleDestroyer.png";
-		public static string RombDestroyer = "rombDestroyer.png";
-		public static string PentagonDestroyer = "pentagonDestroyer.png";
-		public static string HexagonDestroyer = "hexagonDestroyer.png";
-		public static string OctagonDestroyer = "octagonDestroyer.png";
-		public static string CircleLine = "circleLine.png";
-		public static string RombLine = "rombLine.png";
-		public static string PentagonLine = "pentagonLine.png";
-		public static string HexagonLine = "hexagonLine.png";
-		public static string OctagonLine = "octagonLine.png";
+		public static string Font = FontsFolder + "helvetica.otf";
+		public static string GameLogo = TexturesFolder + "gameLogo.png";
+		public static string CircleBomb = TexturesFolder + "circleBomb.png";
+		public static string RombBomb = TexturesFolder + "rombBomb.png";
+		public static string PentagonBomb = TexturesFolder + "pentagonBomb.png";
+		public static string HexagonBomb = TexturesFolder + "hexagonBomb.png";
+		public static string OctagonBomb = TexturesFolder + "octagonBomb.png";
+		public static string CircleDestroyer = TexturesFolder + "circleDestroyer.png";
+		public static string RombDestroyer = TexturesFolder + "rombDestroyer.png";
+		public static string PentagonDestroyer = TexturesFolder + "pentagonDestroyer.png";
+		public static string HexagonDestroyer = TexturesFolder + "hexagonDestroyer.png";
+		public static string OctagonDestroyer = TexturesFolder + "octagonDestroyer.png";
+		public static string CircleLine = TexturesFolder + "circleLine.png";
+		public static string RombLine = TexturesFolder + "rombLine.png";
+		public static string PentagonLine = TexturesFolder + "pentagonLine.png";
+		public static string HexagonLine = TexturesFolder + "hexagonLine.png";
+		public static string OctagonLine = TexturesFolder + "octagonLine.png";
 		public static List<string> Music = new List<string> {
-			"thrii1.ogg", "thrii2.ogg", "thrii3.ogg", "thrii4.ogg"
+			SoundsFolder + "thrii1.ogg", 
+			SoundsFolder + "thrii2.ogg", 
+			SoundsFolder + "thrii3.ogg", 
+			SoundsFolder + "thrii4.ogg"
 		};
-		public static string GemSound = "gem.ogg";
-		public static string DestroyerSound = "destroyer.ogg";
-		public static string BoomSound = "boom.ogg";
+		public static string GemSound = SoundsFolder + "gem.ogg";
+		public static string DestroyerSound = SoundsFolder + "destroyer.ogg";
+		public static string BoomSound = SoundsFolder + "boom.ogg";
 	}
 }
